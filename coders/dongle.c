67ad309ae35ecc2d0853d7a770c52e88ce519445
Ahmed Moutaz El Bakkali Telidi <ael-bakk@c2r1s5.42malaga.com>
2026-04-02 11:24:32 +0200

    e

--- coders/dongle.c
+++ coders/dongle.c
@@ -6,7 +6,7 @@
 /*   By: ael-bakk <ael-bakk@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/01 11:10:15 by ael-bakk          #+#    #+#             */
-/*   Updated: 2026/04/01 16:47:19 by ael-bakk         ###   ########.fr       */
+/*   Updated: 2026/04/02 10:50:52 by ael-bakk         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,49 +16,48 @@
 static void	enqueue_coder(t_dongle *dongle, int coder_id, long priority)
 {
 	t_priority_queue	*q;
-	int					i;
 	t_priority_coder	tmp;
+	int					i;
+	int					parent;
 
 	q = &dongle->wait_queue;
 	if (q->size >= q->capacity)
 		return ;
-	q->coders[q->size].coder_id = coder_id;
-	q->coders[q->size].priority = priority;
-	q->size++;
+	q->coders[q->size++] = (t_priority_coder){coder_id, priority};
 	i = q->size - 1;
-	while (i > 0 && q->coders[i].priority < q->coders[(i - 1) / 2].priority)
+	while (i > 0)
 	{
+		parent = (i - 1) / 2;
+		if (q->coders[i].priority >= q->coders[parent].priority)
+			break ;
 		tmp = q->coders[i];
-		q->coders[i] = q->coders[(i - 1) / 2];
-		q->coders[(i - 1) / 2] = tmp;
-		i = (i - 1) / 2;
+		q->coders[i] = q->coders[parent];
+		q->coders[parent] = tmp;
+		i = parent;
 	}
 }
 
 static int	dequeue_coder(t_dongle *dongle)
 {
 	t_priority_queue	*q;
+	t_priority_coder	tmp;
 	int					result;
 	int					i;
 	int					smallest;
-	t_priority_coder	tmp;
 
 	q = &dongle->wait_queue;
 	if (q->size == 0)
 		return (-1);
 	result = q->coders[0].coder_id;
-	q->coders[0] = q->coders[q->size - 1];
-	q->size--;
+	q->coders[0] = q->coders[--q->size];
 	i = 0;
 	while (2 * i + 1 < q->size)
 	{
-		smallest = i;
-		if (q->coders[2 * i + 1].priority < q->coders[smallest].priority)
-			smallest = 2 * i + 1;
-		if (2 * i + 2 < q->size && q->coders[2 * i
-				+ 2].priority < q->coders[smallest].priority)
+		smallest = 2 * i + 1;
+		if (2 * i + 2 < q->size
+			&& q->coders[2 * i + 2].priority < q->coders[smallest].priority)
 			smallest = 2 * i + 2;
-		if (smallest == i)
+		if (q->coders[i].priority <= q->coders[smallest].priority)
 			break ;
 		tmp = q->coders[i];
 		q->coders[i] = q->coders[smallest];
@@ -82,87 +81,83 @@ static int	is_coder_in_queue(t_dongle *dongle, int coder_id)
 	return (0);
 }
 
-// Releases dongles when called, signals waiting coders
 void	release_dongles(t_coder *coder)
 {
 	t_dongle	*left;
 	t_dongle	*right;
-	long		current_time;
+	long		now;
 
 	left = coder->left_dongle;
 	right = coder->right_dongle;
-	current_time = get_elapsed_ms(coder->sim);
+	now = get_elapsed_ms(coder->sim);
 	pthread_mutex_lock(&left->mutex);
 	left->is_held = 0;
-	left->release_time = current_time;
+	left->release_time = now;
 	pthread_cond_broadcast(&left->cond);
 	pthread_mutex_unlock(&left->mutex);
 	pthread_mutex_lock(&right->mutex);
 	right->is_held = 0;
-	right->release_time = current_time;
+	right->release_time = now;
 	pthread_cond_broadcast(&right->cond);
 	pthread_mutex_unlock(&right->mutex);
 }
 
-// Waits until both dongles are available, then locks them
-void	wait_for_dongles(t_coder *coder)
+void	wait_for_dongles(t_coder *c)
 {
-	t_dongle	*left;
-	t_dongle	*right;
-	long		priority;
-	long		current_time;
+	t_dongle	*l;
+	t_dongle	*r;
+	long		p;
+	long		now;
 
-	if (coder->left_dongle->id < coder->right_dongle->id)
+	if (c->left_dongle->id < c->right_dongle->id)
 	{
-		left = coder->left_dongle;
-		right = coder->right_dongle;
+		l = c->left_dongle;
+		r = c->right_dongle;
 	}
 	else
 	{
-		left = coder->right_dongle;
-		right = coder->left_dongle;
+		l = c->right_dongle;
+		r = c->left_dongle;
 	}
-	if (coder->sim->params.sched == EDF_SCHED)
-		priority = coder->deadline;
+	if (c->sim->params.sched == EDF_SCHED)
+		p = c->deadline;
 	else
-		priority = get_timestamp_ms();
-	pthread_mutex_lock(&left->mutex);
-	current_time = get_elapsed_ms(coder->sim);
-	while (left->is_held || (current_time
-			- left->release_time < coder->sim->params.dongle_cooldown_ms))
+		p = get_timestamp_ms();
+	pthread_mutex_lock(&l->mutex);
+	now = get_elapsed_ms(c->sim);
+	while (l->is_held || now - l->release_time
+		< c->sim->params.dongle_cooldown_ms)
 	{
-		if (!is_coder_in_queue(left, coder->id))
-			enqueue_coder(left, coder->id, priority);
-		pthread_cond_wait(&left->cond, &left->mutex);
-		current_time = get_elapsed_ms(coder->sim);
+		if (!is_coder_in_queue(l, c->id))
+			enqueue_coder(l, c->id, p);
+		pthread_cond_wait(&l->cond, &l->mutex);
+		now = get_elapsed_ms(c->sim);
 	}
-	if (dequeue_coder(left) != coder->id && left->wait_queue.size > 0)
+	if (dequeue_coder(l) != c->id && l->wait_queue.size > 0)
 	{
-		enqueue_coder(left, coder->id, priority);
-		pthread_cond_wait(&left->cond, &left->mutex);
-		current_time = get_elapsed_ms(coder->sim);
+		enqueue_coder(l, c->id, p);
+		pthread_cond_wait(&l->cond, &l->mutex);
 	}
-	left->is_held = 1;
-	pthread_mutex_unlock(&left->mutex);
-	log_taken_dongle(coder->sim, coder);
-	pthread_mutex_lock(&right->mutex);
-	current_time = get_elapsed_ms(coder->sim);
-	while (right->is_held || (current_time
-			- right->release_time < coder->sim->params.dongle_cooldown_ms))
+	l->is_held = 1;
+	pthread_mutex_unlock(&l->mutex);
+	log_taken_dongle(c->sim, c);
+	pthread_mutex_lock(&r->mutex);
+	now = get_elapsed_ms(c->sim);
+	while (r->is_held || now - r->release_time
+		< c->sim->params.dongle_cooldown_ms)
 	{
-		if (!is_coder_in_queue(right, coder->id))
-			enqueue_coder(right, coder->id, priority);
-		pthread_cond_wait(&right->cond, &right->mutex);
-		current_time = get_elapsed_ms(coder->sim);
+		if (!is_coder_in_queue(r, c->id))
+			enqueue_coder(r, c->id, p);
+		pthread_cond_wait(&r->cond, &r->mutex);
+		now = get_elapsed_ms(c->sim);
 	}
-	if (dequeue_coder(right) != coder->id && right->wait_queue.size > 0)
+	if (dequeue_coder(r) != c->id && r->wait_queue.size > 0)
 	{
-		enqueue_coder(right, coder->id, priority);
-		pthread_cond_wait(&right->cond, &right->mutex);
-		current_time = get_elapsed_ms(coder->sim);
+		enqueue_coder(r, c->id, p);
+		pthread_cond_wait(&r->cond, &r->mutex);
 	}
-	right->is_held = 1;
-	pthread_mutex_unlock(&right->mutex);
-	log_taken_dongle(coder->sim, coder);
-	coder->last_compile_start = get_elapsed_ms(coder->sim);
+	r->is_held = 1;
+	pthread_mutex_unlock(&r->mutex);
+	log_taken_dongle(c->sim, c);
+	c->last_compile_start = get_elapsed_ms(c->sim);
 }
